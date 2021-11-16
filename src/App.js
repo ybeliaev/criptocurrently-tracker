d51719a31c0b17e03a2b7f1723a58f3a81e9e7a9
Yurij Beliaev <ybeliaev@gmail.com>
2021-11-16 22:17:58 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import CoinPage from "./components/CoinPage";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+      </div>
+      <Routes>
+        <Route path="/" element={Homepage} />
+        <Route path="/coins/:id" element={CoinPage} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/CoinPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoinPage = () => {
+    return (
+        <div>
+            Coin page
+        </div>
+    );
+};
+
+export default CoinPage;
