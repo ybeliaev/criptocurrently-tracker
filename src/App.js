8129ff95e24fa67cf45e60f3d244ca4b4047f294
Yurij Beliaev <ybeliaev@gmail.com>
2021-11-20 15:42:19 +0200

    add AppBar and Select in Header.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Homepage from "./pages/Homepage";
 import CoinPage from "./components/CoinPage";
 import Header from "./components/Header";
 
+import { styled } from "@mui/material";
+
+const DivStyled = styled("div")({
+  backgroundColor: "#14161a",
+  color: "white",
+  minHeight: "100vh",
+});
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/coins/:id" element={<CoinPage />} />
-      </Routes>
+      <DivStyled>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </DivStyled>
     </BrowserRouter>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 const Header = () => {
-    return (
-        <div>
-            header
-        </div>
-    );
+  return (
+    <AppBar color={"transparent"} position={"static"}>
+      <Container>
+        <Toolbar>
+          <Typography
+            sx={{
+              flex: 1,
+              color: "gold",
+              fontFamily: "Montserrat",
+              fontWeight: "bold",
+              cursor: "pointer",
+            }}
+          >
+            Crypto Tracker
+          </Typography>
+          <Select
+            variant={"outlined"}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={"USD"}
+            label="Age"
+            onChange={(e) => console.log(e.target.value)}
+            sx={{ width: 100, height: 40, marginLeft: 15 }}
+          >
+            <MenuItem value="USD">USD</MenuItem>
+            <MenuItem value="UAH">UAH</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
