38df3f1d72c54452ac96450e527b0193c9b8ce7a
Yurij Beliaev <ybeliaev@gmail.com>
2021-11-27 16:09:01 +0200

    create api, Carousel and add axios

---
+++ src/components/Carousel.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { styled } from "@mui/material";
+
+const DivCarouselStyled = styled("div")({
+  height: "50%",
+  display: "flex",
+  alignItems: "center",
+});
+
+const Carousel = () => {
+  const fetchTrendingCoins = () => {};
+  return <DivCarouselStyled>carousel</DivCarouselStyled>;
+};
+
+export default Carousel;
