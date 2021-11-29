d0e6fc1163b668c2d84f49058353a3aef61ec0d4
Yurij Beliaev <ybeliaev@gmail.com>
2021-11-29 20:53:00 +0200

    create carousel with css, but some style problem

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,15 +1,25 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material";
 import axios from "axios";
 import { TrendingCoins } from "../config/api";
 import { CryptoState } from "../context/CryptoContext";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
+import { numberWithCommas } from "../constants";
 
 const DivCarouselStyled = styled("div")({
   height: "50%",
   display: "flex",
   alignItems: "center",
 });
-
+const linkCarouselItemStyled = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  cursor: "pointer",
+  textTransform: "uppercase",
+  color: "white",
+};
 const Carousel = () => {
   const [trending, setTrending] = useState([]);
   const { currency, symbol } = CryptoState();
@@ -22,7 +32,58 @@ const Carousel = () => {
     fetchTrendingCoins();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currency]);
-  return <DivCarouselStyled>carousel</DivCarouselStyled>;
+
+  const items = trending.map((coin) => {
+    let profit = coin?.price_change_percentage_24h >= 0;
+    return (
+      <Link sx={linkCarouselItemStyled} to={`/coins/${coin.id}`}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+        <span>
+          {coin?.symbol}
+          &nbsp;
+          <span
+            style={{
+              color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+              fontWeight: 500,
+            }}
+          >
+            {profit && "+"}
+            {coin?.price_change_percentage_24h?.toFixed(2)}%
+          </span>
+        </span>
+        <span style={{ fontSize: 22, fontWeight: 500 }}>
+          {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
+      </Link>
+    );
+  });
+
+  return (
+    <DivCarouselStyled>
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        responsive={{
+          0: {
+            items: 2,
+          },
+          512: {
+            items: 4,
+          },
+        }}
+        autoPlay
+        items={items}
+      />
+    </DivCarouselStyled>
+  );
 };
 
 export default Carousel;
