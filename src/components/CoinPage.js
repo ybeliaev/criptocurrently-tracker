6a359b2919c2d556975a9ab93f143da3edca07ce
Yurij Beliaev <ybeliaev@gmail.com>
2021-12-18 17:06:30 +0200

    add some breakpoints css + theme

--- src/components/CoinPage.js
+++ src/components/CoinPage.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CryptoState } from "../context/CryptoContext";
+import axios from "axios";
+import { SingleCoin } from "../config/api";
+import styled from "@emotion/styled";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
 
 const CoinPage = () => {
-    return (
-        <div>
-            Coin page
-        </div>
-    );
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency, symbol } = CryptoState("");
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+
+    setCoin(data);
+  };
+  console.log(coin);
+  const DivContainer = styled("div")(({ theme }) => ({
+    display: "flex",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+    },
+  }));
+  const DivSidebar = styled("div")(({ theme }) => ({
+    width: "30%",
+    [theme.breakpoints.down("md")]: {
+      width: "100%",
+    },
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 25,
+    borderRight: "2px solid grey",
+  }));
+  useEffect(() => {
+    fetchCoin();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <DivContainer>
+        <DivSidebar>Coin page</DivSidebar>
+      </DivContainer>
+    </ThemeProvider>
+  );
 };
 
 export default CoinPage;
