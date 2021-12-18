fed564c20c2bd3781b334fc7073321ba6101bc9d
Yurij Beliaev <ybeliaev@gmail.com>
2021-12-18 17:41:02 +0200

    done sidebar with description coin

--- src/components/CoinPage.js
+++ src/components/CoinPage.js
@@ -6,7 +6,8 @@ import axios from "axios";
 import { SingleCoin } from "../config/api";
 import styled from "@emotion/styled";
 import { ThemeProvider } from "@emotion/react";
-import { createTheme, Typography } from "@mui/material";
+import { createTheme, LinearProgress, Typography } from "@mui/material";
+import { numberWithCommas } from "../constants";
 
 const CoinPage = () => {
   const { id } = useParams();
@@ -38,6 +39,25 @@ const CoinPage = () => {
     marginTop: 25,
     borderRight: "2px solid grey",
   }));
+  const DivMarketData = styled("div")(({ theme }) => ({
+    marketData: {
+      alignSelf: "start",
+      padding: 25,
+      paddingTop: 10,
+      width: "100%",
+      [theme.breakpoints.down("md")]: {
+        display: "flex",
+        justifyContent: "space-around",
+      },
+      [theme.breakpoints.down("sm")]: {
+        flexDirection: "column",
+        alignItems: "center",
+      },
+      [theme.breakpoints.down("xs")]: {
+        alignItems: "start",
+      },
+    },
+  }));
   useEffect(() => {
     fetchCoin();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -47,6 +67,7 @@ const CoinPage = () => {
       mode: "dark",
     },
   });
+  if (!coin) return <LinearProgress style={{ backgroundColor: "gold" }} />;
   return (
     <ThemeProvider theme={darkTheme}>
       <DivContainer>
@@ -70,7 +91,82 @@ const CoinPage = () => {
           <Typography variant="subtitle1">
             {ReactHtmlParser(coin?.description.en.split(". ")[0])}.
           </Typography>
+          <DivMarketData>
+            <span style={{ display: "flex" }}>
+              <Typography
+                variant="h5"
+                sx={{
+                  fontWeight: "bold",
+                  marginBottom: 20,
+                  fontFamily: "Montserrat",
+                }}
+              >
+                Rank:
+              </Typography>
+              &nbsp; &nbsp;
+              <Typography
+                variant="h5"
+                style={{
+                  fontFamily: "Montserrat",
+                }}
+              >
+                {numberWithCommas(coin?.market_cap_rank)}
+              </Typography>
+            </span>
+            <span style={{ display: "flex" }}>
+              <Typography
+                variant="h5"
+                sx={{
+                  fontWeight: "bold",
+                  marginBottom: 20,
+                  fontFamily: "Montserrat",
+                }}
+              >
+                Current Price:
+              </Typography>
+              &nbsp; &nbsp;
+              <Typography
+                variant="h5"
+                style={{
+                  fontFamily: "Montserrat",
+                }}
+              >
+                {symbol}{" "}
+                {numberWithCommas(
+                  coin?.market_data.current_price[currency.toLowerCase()]
+                )}
+              </Typography>
+            </span>
+            <span style={{ display: "flex" }}>
+              <Typography
+                variant="h5"
+                sx={{
+                  fontWeight: "bold",
+                  marginBottom: 20,
+                  fontFamily: "Montserrat",
+                }}
+              >
+                Market Cap:
+              </Typography>
+              &nbsp; &nbsp;
+              <Typography
+                variant="h5"
+                style={{
+                  fontFamily: "Montserrat",
+                }}
+              >
+                {symbol}{" "}
+                {numberWithCommas(
+                  coin?.market_data.market_cap[currency.toLowerCase()]
+                    .toString()
+                    .slice(0, -6)
+                )}
+                M
+              </Typography>
+            </span>
+          </DivMarketData>
         </DivSidebar>
+        {/*<CoinInfo coin={coin} />*/}
       </DivContainer>
     </ThemeProvider>
   );
